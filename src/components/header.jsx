37e6698693e984d50f8ea44a408b870bb7ba1d95
Matthew Beke <mattbeke@gmail.com>
2024-03-02 12:19:39 -0500

    set up header and linked to app.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+import '../styles/header.css';
+
+const styles = {
+    headerStyle: {
+      background: 'red',
+    },
+    headingStyle: {
+      fontSize: '100px',
+    },
+  };
+  
+  function Header() {
+    return (
+      <header style={styles.headerStyle} className="header">
+        <h1 style={styles.headingStyle}>Welcome</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
