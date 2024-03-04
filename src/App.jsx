e15c06a12a57bc2a12562909e6f7fe6def190653
Matthew Beke <mattbeke@gmail.com>
2024-03-03 20:12:03 -0500

    added placeholder content to about page and included styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className='appHeader'>
         <Header />
         <Navbar />
+      </div>
+      <div className='aboutPage'>
+        <div className='mainHeading'>
+          <h1>ABOUT</h1>
         </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='aboutP'>
+          <p>My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me. My name is blank and this is a paragraph about me.</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer className='appFooter'>
+        <Footer />
+      </footer>
     </>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import '../styles/footer.css';
+
+function Footer() {
+    return (
+      <div className='appFooter'>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+
+  export default Footer;
