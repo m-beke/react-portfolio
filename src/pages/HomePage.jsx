81cae4b4e7ae1329af2a650d2d9c83dc852a6349
Matthew Beke <mattbeke@gmail.com>
2024-03-07 09:47:12 -0500

    began adding content to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+import '../styles/home.css';
+
 const HomePage = () => {
   return (
-    <div className='aboutPage'>
+    <>
+    <div className='home-heading'>
       <h1>Home Test</h1>
     </div>
+    <div className="home-cards">
+      <div className="home-about">
+        <Link to='/About'>ABOUT ME</Link>
+      </div>
+      <div className="home-portfolio">
+        <Link to='/Portfolio'>PORTFOLIO</Link>
+      </div>
+      <div className="home-resume">
+        <Link to='/Resume'>RESUME</Link>
+      </div>
+      <div className="home-contact">
+        <Link to='/Contact'>CONTACT ME</Link>
+      </div>
+    </div>
+    </>
   )
 }
 
