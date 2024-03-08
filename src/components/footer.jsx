5e05ec672b13258d5fd1c887b792a61580f05d84
Matthew Beke <mattbeke@gmail.com>
2024-03-07 19:40:15 -0500

    added footer links and styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,11 @@
 import '../styles/footer.css';
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
-      <div className='appFooter'>
-        <h2>Test</h2>
+      <div className='app-footer'>
+        <Link to='https://github.com/m-beke'>GITHUB</Link>
+        <Link to='https://www.linkedin.com/in/matt-beke-a3679740/'>LINKEDIN</Link>
       </div>
     );
   }
