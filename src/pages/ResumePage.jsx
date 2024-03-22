4f4460f156da7cbda2d5563ce2015a56bd71b087
Madeline Beke <mattbeke@gmail.com>
2024-03-21 20:21:34 -0400

    added download link to resume page

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -2,14 +2,12 @@ import '../styles/resume.css';
 import html from '../assets/images/html5.svg';
 import css from '../assets/images/css3.svg';
 import javascript from '../assets/images/javascript.svg';
-import jQuery from '../assets/images/jquery.svg'
 import nodeJS from '../assets/images/nodejs.svg';
 import mongoDB from '../assets/images/mongodb.svg';
 import mySQL from '../assets/images/mysql.svg';
 import sequelize from '../assets/images/sequelize.svg';
-import handlebars from '../assets/images/handlebars.svg';
-import npm from '../assets/images/npm.svg';
 import reactImg from '../assets/images/react.svg';
+import pdf from "../assets/pdf/MB_Coding_Resume.pdf";
 
 const ResumePage = () => {
     return (
@@ -18,22 +16,26 @@ const ResumePage = () => {
                 <div className='resume'>
                     <h1>RESUME</h1>
                 </div>
-                <div className='language-icons'>
-                    <img src={html}></img>
-                    <img src={css}></img>
-                    <img src={javascript}></img>
-                    <img src={jQuery}></img>
-                    <img src={nodeJS}></img>
-                    <img src={handlebars}></img>
-                    <img src={npm}></img>
-                    <img src={mySQL}></img>
-                    <img src={sequelize}></img>
-                    <img src={mongoDB}></img>
-                    <img src={reactImg}></img>
+                <div className='resume-card'>
+                    <div className='language-icons'>
+                        <div className='proficiency'>
+                            <h2>PROFICIENT IN</h2>
+                        </div>
+                        <img src={html}></img>
+                        <img src={css}></img>
+                        <img src={javascript}></img>
+                        <img src={nodeJS}></img>
+                        <img src={mySQL}></img>
+                        <img src={sequelize}></img>
+                        <img src={mongoDB}></img>
+                        <img src={reactImg}></img>
+                    </div>
+                </div>
+                <div className='download-link'>
+                    <a href={pdf} download="MB_Coding_Resume">Download</a>
                 </div>
             </div>
         </>
-
     )
 }
 
