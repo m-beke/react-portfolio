139f347fed779b1fb4dfa369df5da8df0dde78f0
Madeline Beke <mattbeke@gmail.com>
2024-03-28 20:51:26 -0400

    moved contents of about page to home page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
       <nav className="navLinks">
-        <Link to="/About">ABOUT ME</Link>
-        <Link to='/Portfolio'>PORTFOLIO</Link>
+        {/* <Link to="/About">ABOUT ME</Link> */}
+        <Link to='/Portfolio'>PROJECTS</Link>
         <Link to='/Resume'>RESUME</Link>
         <Link to='/Contact'>CONTACT ME</Link>
       </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,38 @@
+import '../styles/about.css';
+import profilePic from "../assets/images/IMG_1448.jpg";
 import { Link } from "react-router-dom";
-import '../styles/home.css';
 
-const HomePage = () => {
+function HomePage() {
   return (
-    <>
-    <div className="home">
-    <div className='home-heading'>
-      <h1>ABOUT</h1>
-      <h1>PORTFOLIO</h1>
-      <h1>RESUME</h1>
-      <h1>CONTACT</h1>
+    <div className='about-page'>
+      <div className='mainHeading'>
+        <h1>ABOUT</h1>
+      </div>
+      <div className='about-card'>
+        <div className='about-content'>
+          <div className='about-p'>
+            <p>My name is Matthew Beke. I am a recent graduate of UCF's Coding Bootcamp. I used to do freelancing work in Film and TV, but am now seeking a career change, and more stable work.</p>
+            <p>Computers have always been a hobby of mine. I've always enjoyed working with them, which eventually led to me discovering a passion for coding. I'm excited to turn that passion into potential job opportunities!</p>
+
+          </div>
+          <div className='about-img'>
+            <img src={profilePic}></img>
+            <div className='about-links'>
+              <div className='about-link-bg'>
+                <Link to='/Portfolio'>PROJECTS</Link>
+              </div>
+              <div className='about-link-bg'>
+                <Link to='/Resume'>RESUME</Link>
+              </div>
+              <div className='about-link-bg'>
+                <Link to='/Contact'>CONTACT ME</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="home-cards">
-      <div className="home-about">
-        <p>Learn more about me!</p>
-        <Link to='/About'>MORE INFO</Link>
-      </div>
-      <div className="home-portfolio">
-        <p>Take a look at the projects I've worked on!</p>
-        <Link to='/Portfolio'>MORE INFO</Link>
-      </div>
-      <div className="home-resume">
-        <p>Check out my qualifications!</p>
-        <Link to='/Resume'>MORE INFO</Link>
-      </div>
-      <div className="home-contact">
-        <p>Give me a call or an email!</p>
-        <Link to='/Contact'>MORE INFO</Link>
-      </div>
-    </div>
-    </div>
-    </>
-  )
+  );
 }
 
 export default HomePage;
