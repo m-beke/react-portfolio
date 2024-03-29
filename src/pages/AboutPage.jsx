2087860f1e666dadb511fd37b47fcd3124c59d9e
Madeline Beke <mattbeke@gmail.com>
2024-03-28 20:42:28 -0400

    updated about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,20 +1,38 @@
 import '../styles/about.css';
+import profilePic from "../assets/images/IMG_1448.jpg";
+import { Link } from "react-router-dom";
 
 function AboutPage() {
-    return (
-      <div className='about-page'>
-        <div className='mainHeading'>
-          <h1>ABOUT</h1>
-        </div>
-        <div className='about-card'>
-        <div className='about-p'>
-          <p>My name is Matthew Beke. I am a recent graduate of UCF's Coding Bootcamp. I used to do freelancing work in Film and TV, but am now seeking a career change, and more stable work.</p>
-          <p>Computers have always been a hobby of mine. I've always enjoyed working with them, which eventually led to me discovering a passion for coding. I'm excited to turn that passion into potential job opportunities!</p>
-          <p>Let's Get to work!</p>
-        </div>
+  return (
+    <div className='about-page'>
+      <div className='mainHeading'>
+        <h1>ABOUT</h1>
+      </div>
+      <div className='about-card'>
+        <div className='about-content'>
+          <div className='about-p'>
+            <p>My name is Matthew Beke. I am a recent graduate of UCF's Coding Bootcamp. I used to do freelancing work in Film and TV, but am now seeking a career change, and more stable work.</p>
+            <p>Computers have always been a hobby of mine. I've always enjoyed working with them, which eventually led to me discovering a passion for coding. I'm excited to turn that passion into potential job opportunities!</p>
+
+          </div>
+          <div className='about-img'>
+            <img src={profilePic}></img>
+            <div className='about-links'>
+              <div className='about-link-bg'>
+                <Link to='/Portfolio'>PROJECTS</Link>
+              </div>
+              <div className='about-link-bg'>
+                <Link to='/Resume'>RESUME</Link>
+              </div>
+              <div className='about-link-bg'>
+                <Link to='/Contact'>CONTACT ME</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default AboutPage;
+export default AboutPage;
