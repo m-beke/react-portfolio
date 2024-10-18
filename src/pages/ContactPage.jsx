a3d92f5cbc13cd8f96d3f81a0e8a05e2a4424953
Madeline Beke <mattbeke@gmail.com>
2024-10-18 14:08:27 -0400

    Began rebuild; Removed old CSS styling

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,15 +2,17 @@ import '../styles/contact.css';
 
 const ContactPage = () => {
     return (
+        <>
         <div className='contact'>
             <h1>CONTACT</h1>
             <div className='contact-card'>
                 <h2>EMAIL:</h2>
-                <a href = "mailto: mattbeke@gmail.com">mattbeke@gmail.com</a>
+                <a href = "mailto: madeline.taylor.beke@gmail.com">madeline.taylor.beke@gmail.com</a>
                 <h2>PHONE NUMBER:</h2>
                 <a href="tel:727-741-5382">(727)-741-5382</a>
             </div>
         </div>
+        </>
     )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ function HomePage() {
       <div className='about-card'>
         <div className='about-content'>
           <div className='about-p'>
-            <p>My name is Matthew Beke. I am a recent graduate of UCF's Coding Bootcamp. I used to do freelancing work in Film and TV, but am now seeking a career change, and more stable work.</p>
+            <p>My name is Madeline Beke. I am a recent graduate of UCF's Coding Bootcamp. I used to do freelancing work in Film and TV, but am now seeking a career change, and more stable work.</p>
             <p>Computers have always been a hobby of mine. I've always enjoyed working with them, which eventually led to me discovering a passion for coding. I'm excited to turn that passion into potential job opportunities!</p>
 
           </div>
