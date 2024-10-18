328b4003d24f76b50577b5516792277508e15ff2
Madeline Beke <mattbeke@gmail.com>
2024-10-18 14:12:03 -0400

    installed and included materialize

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function Footer() {
     return (
       <div className='app-footer'>
-        <Link to='https://github.com/m-beke'>GITHUB</Link>
-        <Link to='https://www.linkedin.com/in/matt-beke-a3679740/'>LINKEDIN</Link>
+        <Link to='https://github.com/m-beke'>Github</Link>
+        <Link to='https://www.linkedin.com/in/matt-beke-a3679740/'>LinkedIn</Link>
       </div>
     );
   }
