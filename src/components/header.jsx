f68195ff6e413b8a0c3b60cf360c63a9c9813b8e
Madeline Beke <mattbeke@gmail.com>
2024-10-20 19:54:58 -0400

    installed react-materialize and fixed navbar/homepage

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,44 @@
 import '../styles/header.css';
 import '../styles/materialize.min.css';
-import '../../utils/materialize.js';
+import '../../utils/materialize.min.js';
 import { Link } from 'react-router-dom';
 import gitIcon from '../assets/images/github-mark-white.png';
 import linkedIcon from '../assets/images/In-White-128@2x.png';
+import { Navbar } from 'react-materialize';
+import { NavItem } from 'react-materialize';
+import { Icon } from 'react-materialize';
+
 
 function Header() {
   return (
     <>
       <nav>
-        <div class="nav-wrapper">
-          <a class="brand-logo"><Link to='/'>Madeline Beke</Link></a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-          <ul class="right hide-on-med-and-down">
-            <li><Link to='/About'>About</Link></li>
-            <li><Link to='/Portfolio'>Projects</Link></li>
-            <li><Link to='/Resume'>Resume</Link></li>
-            <li><Link to='/Contact'>Contact Me</Link></li>
-          </ul>
-          <img src={gitIcon} className='git-icon' to='https://github.com/m-beke'></img>
-          <img src={linkedIcon} className='linked-icon' to='https://www.linkedin.com/in/matt-beke-a3679740/'></img>
-          <ul class="sidenav" id="mobile-demo">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">Javascript</a></li>
-            <li><a href="mobile.html">Mobile</a></li>
-          </ul>
-        </div>
+        <Navbar
+          alignLinks="right"
+          brand={<Link to='/' className="brand-logo">Madeline Beke</Link>}
+          id="mobile-nav"
+          menuIcon={<Icon>menu</Icon>}
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
+        >
+
+
+            <Link to='/About'>About</Link>
+          <Link to='/Portfolio'>Projects</Link>
+          <Link to='/Resume'>Resume</Link>
+          <Link to='/Contact'>Contact Me</Link>
+          <Link to='https://github.com/m-beke'>GitHub</Link>
+        <Link to='https://www.linkedin.com/in/matt-beke-a3679740/'>LinkedIn</Link>
+        </Navbar>
 
       </nav>
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,19 @@
 import '../styles/about.css';
-import "../../utils/materialize.js";
 import profilePic from "../assets/images/IMG_1448.jpg";
 import { Link } from "react-router-dom";
 
 function AboutPage() {
   return (
     <>
-      
+    <div className='about-page'>
+      <div class="carousel">
+        <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"/></a>
+        <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"/></a>
+        <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/250/250/nature/3"/></a>
+        <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/250/250/nature/4"/></a>
+        <a class="carousel-item" href="#five!"><img src="https://lorempixel.com/250/250/nature/5"/></a>
+      </div>
+    </div>
     </>
   );
 }
