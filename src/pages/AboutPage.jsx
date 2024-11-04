219d8c5f86b78b3eb42ee276c1392a035bf0e811
Madeline Beke <mattbeke@gmail.com>
2024-11-04 17:33:23 -0500

    layout changes on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import '../styles/about.css';
 import { assets } from '../../utils/Assets';
 import { Parallax } from 'react-materialize';
+import { Link } from 'react-router-dom';
 
 function AboutPage() {
   return (
@@ -36,11 +37,19 @@ function AboutPage() {
           <div className="row container">
             <div className='p-wrapper'>
               <p className="about-p">
-                Music was one of my first passions. After learning how to play guitar at age 10, I picked the saxophone in middle school. I would go on to attended the Leadership Conservatory for the Arts at Tarpon Springs High School, where I got to perform in the 2013 Macy's Thanksgiving Day Parade, win 1st place in the Bands of America Grand National Championships, and perform with famous contemporary jazz musician Wycliffe Gordon, and current leader of the Count Basie Orchestra, Scotty Barnhart. After graduating in 2015, I decided to pursue another art medium I was passionate about: Film...
+                Music was one of my first passions. After learning how to play guitar at age 10, I picked the saxophone in middle school. I would go on to attended the Leadership Conservatory for the Arts at Tarpon Springs High School, where I got to perform in the 2013 Macy's Thanksgiving Day Parade, win 1st place in the Bands of America Grand National Championships, and perform with famous contemporary jazz musicians Wycliffe Gordon and Scotty Barnhart.
               </p>
             </div>
             <div className='video-wrapper'>
-              <iframe width="378" height="212.4" src="https://www.youtube.com/embed/3Qr0TU4Rj7k?si=Xz4V5XDvL9N4VAxm" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <iframe
+                width="378" height="212.4" src="https://www.youtube.com/embed/3Qr0TU4Rj7k?si=Xz4V5XDvL9N4VAxm" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+              >
+              </iframe>
+            </div>
+            <div className='secondary-p-wrapper'>
+              <p className='about-p-end'>
+                After graduating in 2015, I moved to Orlando, FL, where I would attend Valencia College, and pursue another art medium I was passionate about: Film...
+              </p>
             </div>
           </div>
         </div>
@@ -55,10 +64,12 @@ function AboutPage() {
           <div className="row container">
             <div className='p-wrapper'>
               <p className="about-p">
-                I attended Valencia College, where I got my A.S. degree in Film Production Technology. Once I got my degree in 2018, I moved out to Atlanta, Georgia to start my career as a freelance Production Coordinator. In that time, I worked on numerous film and tv sets, including The Tonight Show with Jimmy Fallon, American Idol, David Makes Man, and more.
+                Once I received my A.S. degree in Film Production Technology in 2018, I moved out to Atlanta, Georgia to start my career as a freelance Production Coordinator. In that time, I worked on numerous film and tv sets, including The Tonight Show with Jimmy Fallon, American Idol, David Makes Man, and more.
               </p>
-              <p className="about-p">
-                When the COVID-19 pandemic hit, film work began to dry up. After the pandemic, that well stayed dry; I had to find a new line of work that would fill me with the same drive that music and film had. That led me to exploring one of my other passions: Computers...
+            </div>
+            <div className='secondary-p-wrapper'>
+              <p className="about-p-end">
+                When the COVID-19 pandemic hit, film work began to dry up. After the pandemic, that well stayed dry; I had to find a new line of work that would fill me with the same drive that music and film had. That search took me back to school, where I would learn how to code...
               </p>
             </div>
           </div>
@@ -74,7 +85,17 @@ function AboutPage() {
           <div className="row container">
             <div className='p-wrapper'>
               <p className="about-p">
-                I decided to go back to school. From 2023-2024, I attended UCF's Coding Bootcamp, where I became certified in HTML5, CSS3, Javascript, MySQL, MongoDB, Express, React, and Node.Js. I am still looking for my niche when it comes to coding, but I'm confident that my passion for it will guide my way as I enter this new profession...
+                 From 2023-2024, I attended the Coding Bootcamp at the University of Central Florida. There, I became certified in HTML5, CSS3, Javascript, MySQL, MongoDB, Express, React, and Node.Js. I utilized these skills to create numerous projects with my classmates. 
+              </p>
+            </div>
+            <div className='link-wrapper'>
+            <Link to='/Portfolio' className='about-portfolio-link'>
+            Check out my Work!
+          </Link>
+          </div>
+            <div className='secondary-p-wrapper'>
+              <p className="about-p-end">
+                I am still looking for my niche when it comes to coding, but I have a hunger for knowledge and a strong desire to learn as much as I can so that I may find that niche. I'm confident that my passion for it will lead me to success and I can't wait to get to work!
               </p>
             </div>
           </div>
